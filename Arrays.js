3d0507fa621d16ada44266e24b159971d31b0986
RuthvikSai2015 <84248537+RuthvikSai2015@users.noreply.github.com>
2023-05-05 21:02:52 +0530

    Arrays

---
+++ Arrays.js
@@ -0,0 +1,35 @@
+// Arrays
+
+let fruits = ["apple","mango","guava","berry","papaya"];
+
+// console.log(fruits);
+
+// //Access the elements in Array
+// //index
+
+// console.log(fruits[0]);
+
+//Add an element into array 
+// fruits[5] = "kiwi";
+
+// console.log(fruits);
+
+// fruits[0] ="orange";
+
+ console.log(fruits);
+
+// Push and Pop 
+
+console.log("push ----" , fruits.push("banana"));
+
+console.log(fruits);
+
+console.log("pop ------", fruits.pop());
+
+// shift and unshift 
+
+fruits.shift();
+
+fruits.unshift("dragon fruit");
+
+console.log(fruits);
