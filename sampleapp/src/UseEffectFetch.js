0c728c76607a0719892880a7da4f3aaf51fd85e8
RuthvikSai2015 <84248537+RuthvikSai2015@users.noreply.github.com>
2023-06-08 20:26:15 +0530

    hooks

--- sampleapp/src/UseEffectFetch.js
+++ sampleapp/src/UseEffectFetch.js
@@ -1,14 +1,30 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
-function UseEffectFetch(){
+function UseEffectFetch() {
+
+    // string = null or ""
+    // boolean = 0
+    //float = 0.0
+    // array  =  []
+    // obj = {}
+    const [content, setContent] = useState([]);
 
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/todos')
-         .then(response => response.json())
-         .then(json => console.log(json))
-    },[]);
+            .then(resp => resp.json())
+            .then(json => setContent(json))
+    }, []);
     return (
+        // <ul>
+        //     {content.map((item) =>
+        //         <li key={item.id}>{item.title}</li>
+        //     )
+        //     }
+        // </ul>
         <>
+          <button>Posts</button>
+          <button>Users</button>
+          <button>Todos</button>
         </>
     )
 }
