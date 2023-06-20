82d221e3467262497cc8165cd56557ba871b2612
RuthvikSai2015 <84248537+RuthvikSai2015@users.noreply.github.com>
2023-06-20 20:21:55 +0530

    React Router

--- sampleapp/src/ReactRouterExample.js
+++ sampleapp/src/ReactRouterExample.js
@@ -1,24 +1,75 @@
-import { Route , Routes} from 'react-router-dom';
+import { Route, Routes, Link ,useNavigate} from 'react-router-dom';
+import {useEffect,useState} from "react";
 function Home() {
     return <h1> Home Component</h1>
 }
 function About() {
     return <h2> About Component </h2>
 }
-function Blog(){
+function Blog() {
     return <h2> Blog </h2>
 }
-function NotFound(){
-    return <h2> Not Found Component  </h2>
+function NotFound() {
+    const navigate = useNavigate();
+    return (
+        <>
+            <h2> Not Found Component  </h2>
+            {/* <Link to="/" >Go Back to Home </Link> */}
+             <button onClick={()=> navigate('/') } > Go Back to Home</button> 
+        </>
+    )
+}
+function NavBar() {
+    return (
+        <nav>
+            <Link to="/" >Home |</Link>
+            <Link to="/about" >About |</Link>
+            <Link to="/blog" >Blog |</Link>
+            <Link to="/posts" >Posts</Link>
+        </nav>
+    )
+}
+function Posts(){
+    const [content,setContent]=useState([]);
+    useEffect(() => {
+        fetch(`https://jsonplaceholder.typicode.com/posts`)
+            .then(resp => resp.json())
+            .then(json => setContent(json))
+    }, []);
+    return (
+        <ul>
+            {
+                content.map((post) => {
+                    return (
+                        <> 
+                        <li><Link to={`/posts/${post.id}`}>{post.id}</Link></li>
+                        </>
+                    )
+                }
+                  
+                )
+            }
+        </ul>
+    )
+}
+function Post(){
+    return (
+        <>
+        </>
+    )
 }
 function ReactRouterExample() {
     return (
         <>
+            <NavBar />
             <Routes>
-                <Route path="/" element={<Home/>}></Route>
-                <Route path="/about" element={<About/>}></Route>
-                <Route path="/blog" element={<Blog/>}></Route>
-                <Route path="*" element={<NotFound/>}></Route>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="/about" element={<About />}></Route>
+                <Route path="/blog" element={<Blog />}/>
+                <Route path="/posts" element={<Posts/>}>
+                  <Route path=":id" element = {<Post/>}/>
+                </Route>
+                <Route path="*" element={<NotFound />}></Route>
             </Routes>
         </>
     )
